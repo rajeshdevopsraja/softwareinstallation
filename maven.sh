e96084dd0bb9b51a9db0284b884deaa8b0cde612
rajeshdevopsraja <120709883+rajeshdevopsraja@users.noreply.github.com>
2023-01-29 00:28:15 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd /opt
+pwd
 sudo unzip apache-maven-3.8.7-bin.zip
 export M2_HOME=/opt/apache-maven-3.8.7/bin
-mvn -version
+#mvn -version
 # Download Maven 3.8.7 version
 #sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.zip
 # Unzip apache maven 3.8.7
