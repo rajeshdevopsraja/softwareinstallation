a170f2235d56f680774f9a9d46fc0256d3f5e5a6
rajeshdevopsraja <120709883+rajeshdevopsraja@users.noreply.github.com>
2023-01-29 00:09:03 +0530

    Updated
    added

--- maven.sh
+++ maven.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /opt
+# Download Maven 3.8.7 version
 sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.zip
 sudo unzip apache-maven-3.8.7-bin.zip
-export MAVEN_HOME=/opt/apache-maven-3.8.7/bin
+# Set Maven global variable
+export M2_HOME=/opt/apache-maven-3.8.7/bin
 mvn -version
